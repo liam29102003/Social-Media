d4204d595a1964cb9145fdbf514ba65946e5d19b
Liam <lkhant616@gmail.com>
2024-07-21 21:32:34 +0630

    Complete

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ import path from 'path';
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
-
+const __dirname = path.resolve();
 app.use(express.json({ limit: "5mb" })); // to parse req.body
 app.use(express.urlencoded({ extended: true })); //to parse form data
 app.use(cookieParser());
@@ -20,9 +20,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 app.listen(PORT, () => {
     console.log("Server is running on port " + PORT);
     connecttoMongoDB();
